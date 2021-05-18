f909ff54f1041e9a7d52e80f9d61cc3100bd555a
Truong Thi Bich Chi <truongthibichchi2509@gmail.com>
2021-05-19 00:11:22 +0700

    decorator

---
+++ decorator/log_decorator_with_param_using_class.py
@@ -0,0 +1,57 @@
+class log:
+    def __init__(self, to='console', file_path='log'):
+        if to == 'console':
+            self.log_func = print
+        else:
+            log_file = open(file_path, 'rt')
+            self.log_func = log_file.write
+
+    def __call__(self, func):
+        def _wrapper(*args, **kwargs):
+            self.log_func(f'input của hàm {func} là: {args} {kwargs}')
+            try:
+                result = func(*args, **kwargs)
+                self.log_func(f'output của hàm {func} là {result}')
+            except BaseException as exc:
+                self.log_func(
+                    f'hàm {func} văng lỗi: {exc}'
+                )
+                raise exc
+
+        return _wrapper
+
+
+class DBConnector:
+    instance = None
+
+    def __init__(
+            self,
+            db_conn_string,
+    ):
+        self.db_conn_string = db_conn_string
+        self.conn = {
+            'db': db_conn_string
+        }
+
+    @log()
+    def query(self, sql):
+        return self.conn['db'] + sql
+
+    @log()
+    def select(self, sql):
+        return sql
+
+
+@log(to='console')
+def tong(a, b, tong_so_am=False):
+    if (a < 0 or b < 0) and not tong_so_am:
+        raise ValueError(
+            'Hàm này không thích tính tổng của số âm'
+        )
+
+    return a + b
+
+
+tong(1, 2)
+tong(-1, 2)
+tong(-1, 2, tong_so_am=True)
